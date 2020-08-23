eaf49bfa8d7a59c6409dfe92d7cb3c0e5829d209
Ranjani <vranju95@gmail.com>
2020-08-23 09:45:23 -0500

    handle incoming POST data
    moved all the logic to `request.on(end)` because if outside the response
    is already sent by the time request ends

--- src/index.js
+++ src/index.js
@@ -1,26 +1,38 @@
 const http = require('http');
 
-const user = {
-    name: 'Ranjani',
-    age: 25
-};
+let user = {};
 const getJsonString = (obj) => JSON.stringify(obj, null, '  ');
 
-const server = http.createServer(function(request, response) {
+const server = http.createServer();
+
+server.on('request', function(request, response) {
     console.log('Handling', request.method, request.url);
-    if (request.url === '/') {
-        response.writeHead(200, { 'Content-Type': 'text/html' })
-        response.write('<h1>Hello</h1>');
-    } else if (request.url === '/about') {
-        response.write('About Me');
-    } else if (request.url === '/user') {
-        response.writeHead(200, { 'Content-Type': 'application/json' })
-        response.write(getJsonString(user));
-    } else {
-        response.writeHead(404);
-        response.write('Not Found');
-    }
-    response.end();
-});
+
+    let body = [];
+    request.on('data', function(chunk) {
+        body.push(chunk);
+    });
+    request.on('end', function() {
+        body = Buffer.concat(body).toString();
+        body = body.length ? JSON.parse(body) : {};
+
+        if (request.url === '/') {
+            response.writeHead(200, { 'Content-Type': 'text/html' })
+            response.write('<h1>Hello</h1>');
+        } else if (request.url === '/about') {
+            response.write('About Me');
+        } else if (request.url === '/user' && request.method === 'GET') {
+            response.writeHead(200, { 'Content-Type': 'application/json' })
+            response.write(getJsonString(user));
+        } else if (request.url === '/user' && request.method === 'POST') {
+            user = body;
+            response.write('Saved');
+        } else {
+            response.writeHead(404);
+            response.write('Not Found');
+        }
+        response.end();
+    });
+})
 
 server.listen(8080);
