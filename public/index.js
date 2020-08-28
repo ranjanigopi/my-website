94814bdbb672ff1a68b87567c9c78b2442c76734
Ranjani <vranju95@gmail.com>
2020-08-27 19:48:43 -0500

    create react components using `React.createElement`
    and render using ReactDOM

--- public/index.js
+++ public/index.js
@@ -1,1 +1,23 @@
-console.log(React);
+const Greeting = (props) => React.createElement(
+    'span', { className: 'greeting' },
+    'First react code ' + props.content
+);
+
+const Home = () => React.createElement(
+    'div', { id: 'main-page' }, [
+        'hello',
+        React.createElement(
+            Greeting, { content: 'yay!' }
+        ),
+        React.createElement(
+            Greeting, { content: 'yaVayy!' }
+        ),
+    ]
+)
+
+ReactDOM.render(
+    React.createElement(
+        Home
+    ),
+    document.getElementById('main')
+)
