adc2a1e1aad049f76c7fa84835c41e30a280fd6c
Ranjani <vranju95@gmail.com>
2020-08-29 20:52:17 -0500

    tell webpack to load css files
    since webpack understands only js, we import css fles through js
    when webpack sees a css file it will call the css-loader we specified
    
    css-loader converts the css file into a js string
    since this is not usable, we use MiniCssExtractPlugin to extract
    that js code to a css file
    
    more info: https://webpack.js.org/plugins/mini-css-extract-plugin/

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,5 @@
 import { Home } from './components/home';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 ReactDOM.render(<Home/>, document.getElementById('main'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+    devtool: false,
     entry: './src/client/index.js',
     output: {
         path: path.join(__dirname, 'public'),
@@ -9,13 +11,21 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /.js$/,
-            use: 'babel-loader'
-        }]
+                test: /.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/client/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'index.css'
         })
     ]
 }
